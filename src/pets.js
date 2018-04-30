b25f501b582a8b47d2cf9d33b71d5b3c01b53b8f
ivana <ivanafustar@hotmail.com>
2018-04-30 14:57:46 +0200

    adding json file

---
+++ src/pets.js
@@ -0,0 +1,188 @@
+const pets = [
+    {
+        "id": 1622147641,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "doggie",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    },
+    {
+        "id": 1622147642,
+        "name": "Doggo",
+        "photoUrls": [],
+        "tags": [],
+        "status": "available"
+    },
+    {
+        "id": 1622147643,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "Doggo",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    },
+    {
+        "id": 1622147644,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "Dog",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    },
+    {
+        "id": 1622147646,
+        "name": "Doggo",
+        "photoUrls": [],
+        "tags": [],
+        "status": "available"
+    },
+    {
+        "id": 1622147647,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "Doggo",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    },
+    {
+        "id": 1622147648,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "Dog",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    },
+    {
+        "id": 1622147649,
+        "name": "Doggo",
+        "photoUrls": [],
+        "tags": [],
+        "status": "available"
+    },
+    {
+        "id": 1622147650,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "Doggo",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    },
+    {
+        "id": 1622147651,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "Dog",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    },
+    {
+        "id": 1622147653,
+        "category": {
+            "id": 0,
+            "name": "test"
+        },
+        "name": "doggie",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    },
+    {
+        "id": 1622147655,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "hello",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    }];
+
+module.exports = {
+    'pets': pets
+};
